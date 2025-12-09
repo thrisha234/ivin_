ec9687c8e78d6caec3171c7fbe41e2bbf05689fc
Your Name <your@email.com>
2025-12-09 14:20:31 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -652,4 +652,5 @@ def download_minutes_docx(meeting_id):
 # RUN SERVER
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
